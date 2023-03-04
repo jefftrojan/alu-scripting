6c71badbbc9f782c2b85f5f8c3babf0f7d6ace8a
Jeff Trojan <j.dauda@alustudent.com>
2023-03-05 00:42:44 +0200

    Pycode Style

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Return number of subscriber for a given subreddit."""
-
+""" Returns the number of subscribers for a given subreddit."""
 import requests
 
-URL = 'https://'
 
-""" 
-
-    Function to get the number of subscribers 
-
-"""
 def number_of_subscribers(subreddit):
     # Set custom user-agent header
-    headers = {'User-Agent': 'ALU-scripting API 0.1'}
+    headers = {"User-Agent": "ALU-scripting API 0.1"}
     url = f"https://www.reddit.com/r/{subreddit}.json"
 
     try:
-        response = requests.get(url, headers=headers, timeout=30, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                timeout=30, allow_redirects=False)
+
     except requests.exceptions.Timeout:
         return "The request Timed out"
 
     if response.status_code == 200:
         json_data = response.json()
-        subscriber_number = json_data.get('data').get('children')[0].get('data').get('subreddit_subscribers')
+        subscriber_number = (
+            json_data.get("data")
+            .get("children")[0]
+            .get("data")
+            .get("subreddit_subscribers")
+        )
         return subscriber_number
     elif response.status_code == 404:
         return 0
