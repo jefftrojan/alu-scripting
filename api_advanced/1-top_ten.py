0352eb08b8c4f8862bdfd07176b6ee2655212fac
Jeff Trojan <j.dauda@alustudent.com>
2023-03-05 02:42:46 +0200

    1 top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,18 +4,19 @@ import requests
 
 
 def top_ten(subreddit):
+    """ Set a custom header user-agent """
     headers = {"User-Agent": "ALU scriptting 1.0"}
-    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    url = f"https://reddit.com/r/{subreddit}.json"
 
     try:
         response = requests.get(url, headers=headers, timeout=30, allow_redirects=False)
     except requests.exceptions.Timeout:
         return "Request Timeout! "
 
-    if response.status_code != 200:
-        print(None)
+    if response.status_code == 200:
+        json_data = response.json()
+        for posts in range(10):
+            print(json_data.get("data").get("children")[posts].get("title"))
 
     else:
-        json_data = response.json()
-        for i in range(10):
-            print(json_data.get("data").get("children").get("title"))
+        print(None)
