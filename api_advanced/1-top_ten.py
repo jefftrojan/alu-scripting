5a2b72fe54bae458a81e8bd464596ce05273dd20
Jeff <j.dauda@alustudent.com>
2023-03-10 17:43:35 +0000

    1 top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -17,7 +17,7 @@ def top_ten(subreddit):
     if response.status_code == 200:
         json_data = response.json()
         for posts in range(10):
-            print(json_data.get("data").get("children")[posts].get("title"))
+            print(json_data.get("data").get("children")[posts].get("data").get("title"))
 
     else:
         print(None)
